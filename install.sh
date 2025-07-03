5639927b52cd4a4788284bbb8ad28ca06fc23d0a
AstroKeith <77962754+AstroKeith@users.noreply.github.com>
2025-07-03 10:16:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -77,7 +77,8 @@ echo "installing Tetra and its databases"
 echo "*****************************************************************************"
 sudo -u efinder git clone https://github.com/esa/tetra3.git
 cd tetra3
-venv-efinder/bin/python /home/efinder/venv-efinder/bin/pip install .
+/home/efinder/venv-efinder/bin/pip install .
+cd $HOME
 sudo venv-efinder/bin/gdown  --output /home/efinder/venv-efinder/lib/python3.11/site-packages/tetra3/data --folder https://drive.google.com/drive/folders/1uxbdttpg0Dpp8OuYUDY9arYoeglfZzcX
 
 cd $HOME
