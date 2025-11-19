b4e936017c7ae8bbc121f1f3a7edff4eb4a1fe12
AstroKeith <77962754+AstroKeith@users.noreply.github.com>
2025-11-19 11:08:05 +0000

    Add files via upload

--- Solver/RPICamera_Nexus_4.py
+++ Solver/RPICamera_Nexus_4.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from picamera2 import Picamera2
 import numpy as np
+import time
 
+# upadated 18/11/2025 to include buffer = 2, speeds up capture x2
 class RPICamera():
     """The camera class for RPI cameras.  Implements the CameraInterface interface."""
 
@@ -11,7 +13,7 @@ class RPICamera():
         handpad (Display): The link to the handpad"""
         self.home_path = str(Path.home())
         self.picam2 = Picamera2()
-        self.camera_config = self.picam2.create_still_configuration({"size":(960,760),"format":"YUV420"},)
+        self.camera_config = self.picam2.create_still_configuration({"size":(960,760),"format":"YUV420"},buffer_count=2)
         self.picam2.configure(self.camera_config)
 
     
@@ -19,7 +21,12 @@ class RPICamera():
         exp = int(exposure_time * 1000000)
         gn = int(float(gain))
         self.picam2.stop()
-        self.picam2.set_controls({"ExposureTime": exp, "AnalogueGain": gn})
+        self.picam2.set_controls({
+            "AeEnable": False,
+            "AwbEnable": False,
+            "ExposureTime": exp, 
+            "AnalogueGain": gn
+            })        
         self.picam2.start()
 
     def capture(
@@ -40,7 +47,10 @@ class RPICamera():
         elif offset == True and hemi == 'S':
             return np.load('/home/efinder/Solver/rigelK.npy')
         else:
+            #print(time.time())
             array = np.array(self.picam2.capture_array())
+            #print(time.time())
             y = array[0:760,0:960]
+            #print(time.time())
             return y
 
